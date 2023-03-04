7a0aff10af3139705f00c16aa2705a0462ba8f42
Sami Elsayed <90798961+selsayed25@users.noreply.github.com>
2023-03-04 02:36:26 +0000

    add: all gatsby files

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `Streamflow`,
+    siteUrl: `https://www.yourdomain.tld`,
+  },
+  plugins: [],
+}
